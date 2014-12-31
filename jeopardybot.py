c36cae83b695e8eef5c551965f73e4a29a09cc20
dannysepler <dsepler@ufl.edu>
2014-12-31 03:44:03 -0500

    renamed python file

--- jeopardybot.py
+++ jeopardybot.py
@@ -5,14 +5,12 @@ import tweepy, time, sys, csv, random, selenium
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from random import randint
-
-# argfile = str(sys.argv[1])
  
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY 	= ''
+CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
-ACCESS_KEY 		= ''
-ACCESS_SECRET 	= ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
