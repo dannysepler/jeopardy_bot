cb7c3bb01fd3205e4ca8eef143ba91a08e27f1e9
dannysepler <dsepler@ufl.edu>
2014-12-31 14:01:07 -0500

    now, i log into pastebin

--- jeopardybot.py
+++ jeopardybot.py
@@ -7,10 +7,10 @@ from selenium.common.exceptions import NoSuchElementException
 from random import randint
  
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_KEY = ''
-ACCESS_SECRET = ''
+CONSUMER_KEY = 'iq8cq3IAWBAfxjZORQ705o6zD'
+CONSUMER_SECRET = 'iKNSC7ubwyeIpjmiZFQemXpAif4B7PuKM0kD7J0JjXearXy6n7'
+ACCESS_KEY = '2945080696-Dz6Q1BDpHqBwXuvvonexDBaJ7Pa2592oNVH4p5I'
+ACCESS_SECRET = '3uND5IeUryleHBnDRwYbdzsrWe4fZiNxxgRSyNYePsmsw'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
@@ -20,9 +20,20 @@ api = tweepy.API(auth)
 # Show Number, Air Date, Round, Category, Value, Question, Answer
 # -----
 
+def log_into_pastebin(browser):
+	browser.find_element_by_xpath('//*[@id="content_left"]/div[4]/div[2]/a[2]').click()
+
+	browser.find_element_by_xpath('//*[@id="myform"]/div/div[1]/div[2]/input').send_keys('jeopardybot')
+	browser.find_element_by_xpath('//*[@id="myform"]/div/div[2]/div[2]/input').send_keys('jeopardybotpastebin')
+	browser.find_element_by_xpath('//*[@id="myform"]/div/div[3]/div[2]/input').click()
+
+	browser.find_element_by_xpath('//*[@id="header_bottom"]/div/a[1]').click()
+
 def post_in_pastebin(full_q):
 	browser = webdriver.Chrome()
 	browser.get('http://pastebin.com')
+
+	log_into_pastebin(browser)
 
 	# populate + send
 	browser.find_element_by_xpath('//*[@id="paste_code"]').send_keys(
@@ -37,7 +48,7 @@ def post_in_pastebin(full_q):
 	browser.find_element_by_xpath('//*[@id="submit"]').click()
 
 	# return link
-	url = browser.find_element_by_xpath('//*[@id="content_left"]/div[1]/div[3]/div[3]/a[2]').get_attribute('href')
+	url = browser.find_element_by_xpath('//*[@id="content_left"]/div[1]/div[3]/div[3]/a[4]').get_attribute('href')
 	browser.close()
 
 	return url
